6db45c6230da4abbc8ffc83eb2ccc9f94e05e9a1
Arjun-Senthilvel <senthilvel.bagavath@gmail.com>
2020-01-18 20:00:18 +0530

    Fixing collide function

--- sketch.js
+++ sketch.js
@@ -1,20 +1,27 @@
 var fixedRectangle, movingRectangle;
+
 function setup() {
-  createCanvas(1200,800);
-  fixedRectangle = createSprite(800,400,50,90)
-  movingRectangle = createSprite(600,400,40,80)
+  createCanvas(1200, 800);
+  fixedRectangle = createSprite(800, 400, 50, 90);
+  movingRectangle = createSprite(600, 400, 40, 80);
   fixedRectangle.shapeColor = "green";
   movingRectangle.shapeColor = "blue";
 }
 
-
 function draw() {
-  background(255,255,255);  
+  background(255, 255, 255);
   movingRectangle.x = World.mouseX;
   movingRectangle.y = World.mouseY;
-  if(movingRectangle.x - fixedRectangle.x < fixedRectangle.width/2 + movingRectangle.width/2 
-    && fixedRectangle.x - movingRectangle.x < fixedRectangle.width/2 + movingRectangle.width/2) {
-
+  if (
+    (movingRectangle.x - fixedRectangle.x <=
+      fixedRectangle.width / 2 + movingRectangle.width / 2 &&
+      fixedRectangle.x - movingRectangle.x <=
+        fixedRectangle.width / 2 + movingRectangle.width / 2) ||
+    (movingRectangle.y - fixedRectangle.y <=
+      fixedRectangle.length / 2 + movingRectangle.length / 2 &&
+      fixedRectangle.y - movingRectangle.y <=
+        fixedRectangle.length / 2 + movingRectangle.length / 2)
+  ) {
     movingRectangle.shapeColor = "red";
     fixedRectangle.shapeColor = "red";
   } else {
